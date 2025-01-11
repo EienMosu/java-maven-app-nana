413a697fb90ee23f859c0144c9916a531db87e05
EienMosu <voodoochild9293@hotmail.com>
2025-01-11 09:03:20 +0000

    dev branch created

--- script.groovy
+++ script.groovy
@@ -1,9 +1,9 @@
 def buildApp() {
-    echo 'building the application STAGE...'
+    echo 'building the application DEV...'
 }
 
 def deployApp() {
-    echo "Deploying the application STAGE..."
+    echo "Deploying the application DEV..."
     echo "deploying version ${params.VERSION}"
 }
 return this
